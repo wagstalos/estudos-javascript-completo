dac438a4cf99683ea6a352ec728db29535627237
Wagner Paulo <wagstalos@hotmail.com>
2023-03-11 11:02:46 -0300

    feat: added array-object

--- functions/array-object-for.js
+++ functions/array-object-for.js
@@ -1,16 +1,17 @@
 const pessoas = [
-  {nome: 'João',
-   idade: 34
-  },
-  {nome: 'maria',
-  idade: 2
- },
- {nome: 'Neusa',
- idade: 33
-}
+  { nome: "João", idade: 34 },
+  { nome: "maria", idade: 2 },
+  { nome: "Neusa", idade: 33 },
 ];
 
+// for(let i=0; i < pessoas.length; i ++){
+//   console.log(`Meu nome é ${pessoas[i].nome} e tenho ${pessoas[i].idade}`);
+// }
 
-for(let i=0; i < pessoas.length; i ++){
-  console.log(`Meu nome é ${pessoas[i].nome} e tenho ${pessoas[i].idade}`);
+pessoas.forEach(updtStudents);
+
+function updtStudents(item, index, arr) {
+  arr[index] = "Hello " + item.nome;
 }
+
+console.log(pessoas);
--- functions/array-object.js
+++ functions/array-object.js
@@ -3,51 +3,29 @@ const cars = [
     brand: "Renault",
     name: "Duster",
     year: 2017,
-    price: (60000.0).toLocaleString("pt-br", {
-      style: "currency",
-      currency: "BRL",
-    }),
+    price: 60000.0
   },
   {
     brand: "Volks",
     name: "Polo",
     year: 2020,
-    price: (80000.0).toLocaleString("pt-br", {
-      style: "currency",
-      currency: "BRL",
-    }),
+    price: 80000.0
   },
   {
     brand: "GM",
     name: "Onix",
     year: 2022,
-    price: (90000.0).toLocaleString("pt-br", {
-      style: "currency",
-      currency: "BRL",
-    }),
+    price: 90000.0
   },
 ];
 
-// console.table(cars);
-// console.log(cars);
+// const getCars = (i) => {
+//   return cars[i].price.toLocaleString("pt-br", {
+//     style: "currency",
+//     currency: "BRL",
+//   });
+// };
 
-// var f = cars[0].price.toLocaleString("pt-br", {
-//   style: "currency",
-//   currency: "BRL",
-// });
-
-// console.log(f);
-// for (var i = 0, totalCars = cars.length; i < totalCars; i++) {
-//   console.log(cars[i]);
-// }
-
-const getCars = (i) => {
-  return cars[i].price.toLocaleString("pt-br", {
-    style: "currency",
-    currency: "BRL",
-  });
-};
-
-cars.map((car, i) => console.table(getCars(i)));
+// cars.map((car, i) => console.table(getCars(i)));
 
 console.table(cars);
